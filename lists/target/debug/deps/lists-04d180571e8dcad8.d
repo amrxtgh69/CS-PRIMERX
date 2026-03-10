9d842ee72b280602ef81bfc25751b59c399faf5b
amrxtgh <ghimireamrit619@gmail.com>
2026-03-10 22:27:03 +0545

    too many linked list written by gay

---
+++ lists/target/debug/deps/lists-04d180571e8dcad8.d
@@ -0,0 +1,6 @@
+/home/amrxtgh/CS-PRIMERX/lists/target/debug/deps/lists-04d180571e8dcad8.d: src/lib.rs src/first.rs
+
+/home/amrxtgh/CS-PRIMERX/lists/target/debug/deps/liblists-04d180571e8dcad8.rmeta: src/lib.rs src/first.rs
+
+src/lib.rs:
+src/first.rs:
---
+++ lists/target/debug/deps/lists-2c07d47f1345ba30.d
@@ -0,0 +1,8 @@
+/home/amrxtgh/CS-PRIMERX/lists/target/debug/deps/lists-2c07d47f1345ba30.d: src/lib.rs src/first.rs
+
+/home/amrxtgh/CS-PRIMERX/lists/target/debug/deps/liblists-2c07d47f1345ba30.rlib: src/lib.rs src/first.rs
+
+/home/amrxtgh/CS-PRIMERX/lists/target/debug/deps/liblists-2c07d47f1345ba30.rmeta: src/lib.rs src/first.rs
+
+src/lib.rs:
+src/first.rs:
