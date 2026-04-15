13c338c462416a0d1ba4945b4adb41b278efe46f
amrxtgh <ghimireamrit619@gmail.com>
2026-04-15 22:59:32 +0545

    crappy

--- systems-programming/Machinelevel_Representation/dm.c
+++
@@ -1,8 +0,0 @@
-// procedure parameters xp and y are stored in %rdi and %rsi
-long exchange(long *xp, long y) {
-  // reads x from the memory and stores in %rax cause it it long return type 
-  long x = *xp;
-  // write y to the memory location designated by xp in %rdi 
-  *xp = y;
-  return y;
-}
